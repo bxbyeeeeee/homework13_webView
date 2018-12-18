32a7a62e0b8030c5545a0dda47e9f0c3e4a971e6
student <student@imac-19.local>
2018-12-18 13:57:52 +0800

    finish

--- webView/ViewController.swift
+++ webView/ViewController.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var urlTF: UITextField!
+    @IBOutlet weak var webView: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if let url = URL(string: "https://m.weibo.cn"){
+            webView.load(URLRequest(url:url))
+        }
     }
-
-
+    @IBAction func back(_ sender: Any) {
+        webView.goBack()
+    }
+    
+    @IBAction func forward(_ sender: Any) {
+        webView.goForward()
+    }
+    
+    @IBAction func reload(_ sender: Any) {
+        webView.reload()
+    }
+    
+    @IBAction func go(_ sender: Any) {
+        if let url = URL(string: urlTF.text!){
+            webView.load(URLRequest(url:url))
+        }
+    }
+    
 }
 
